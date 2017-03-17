8a32c3f0fc031e9e905d6043fdf893c1a5ecd89d
QuinnBischoff <quinn.qb@gmail.com>
2017-03-17 13:13:43 -0600

    images now working, static maps api in progress

--- requestHandler/RequestProcessor.java
+++ requestHandler/RequestProcessor.java
@@ -11,15 +11,20 @@ import javax.ws.rs.core.UriInfo;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.omg.CORBA.portable.InputStream;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+//google api key: AIzaSyCBPC1jeKHhkYOyZHgvX0wYJUORm4nN69E
+
 @Path("/")
 public class RequestProcessor {
+	
 	@GET
-	@Produces("application/json")
+	@Produces({"application/json", "image/png"})
 	public Response lookupFunction(@Context UriInfo ui) throws JSONException, UnirestException{
 		MultivaluedMap<String, String> parameterMap = ui.getQueryParameters();
 		String auroraString = new String("http://api.auroras.live/v1/?type=");
@@ -38,12 +43,22 @@ public class RequestProcessor {
 		}
 		
 		System.out.println(auroraString);
+		
+		if((parameterMap.get("type").get(0).equals("images" ) && !parameterMap.containsKey("action")) || parameterMap.get("type").get(0).equals("embed" ))
+		{
+			HttpResponse<java.io.InputStream> imgResponse = Unirest.get(auroraString).header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asBinary();
+			return Response.status(200).entity(imgResponse.getBody()).type("image/png").build();
+		}
+		if(parameterMap.get("type").get(0).equals("googlemap"))
+		{
+			HttpResponse<java.io.InputStream> imgResponse = Unirest.get("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318%20&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyCBPC1jeKHhkYOyZHgvX0wYJUORm4nN69E").header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asBinary();
+			return Response.status(200).entity(imgResponse.getBody()).type("image/png").build();
+		}
+		else{
 		JSONObject jsonObject = new JSONObject();
 		HttpResponse<JsonNode> response = Unirest.get(auroraString).header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0").asJson();
-		
 		jsonObject = response.getBody().getObject();
-		
-		return Response.status(200).entity(response.getBody().toString()).build();
+		return Response.status(200).entity(response.getBody().toString()).type("application/json").build();
+		}
 	}
-
 }
